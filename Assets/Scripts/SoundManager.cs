a58921c8d9fac9a86224402a0b7ac7b86ee96b76
Stephen Phillips <stephen.phillips@outplay.com>
2018-12-13 17:17:49 +0000

    Added Unity project files for new resources.

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    readonly float bps = 60.0f / 200.0f;
+    float m_elapsed;
+
+    public event EventHandler OnBeat;
+
+    public SoundManager()
+    {
+        m_elapsed = 0.0f;
+    }
+
+    void Update()
+    {
+        m_elapsed += Time.deltaTime;
+        if (m_elapsed >= bps)
+        {
+            m_elapsed -= bps;
+            OnBeat(this, null);
+        }
+    }
+}
