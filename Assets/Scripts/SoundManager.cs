9a46291d8b466a9fbf1a4f1934c9f0609fda5ff0
Stephen Phillips <stephen.phillips.me@gmail.com>
2018-12-18 05:27:43 +0000

    Updated SoundManager to read audio source track time.

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -3,15 +3,18 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    [Zenject.Inject]
+    public AudioSource audioSource;
+
     readonly float bps = 0.45f;
     readonly float halfBPS = 0.45f * 0.5f;
-    float m_elapsed;
+    float m_lastSeenTime;
 
     public event EventHandler OnBeat;
 
     public SoundManager()
     {
-        m_elapsed = 0.0f;
+        m_lastSeenTime = 0.0f;
     }
 
     public float GetBeatLength()
@@ -21,16 +24,18 @@ public class SoundManager : MonoBehaviour
 
     public float TimeOffBeat()
     {
-        return halfBPS - Math.Abs(halfBPS - m_elapsed);
+        return halfBPS - Math.Abs(halfBPS - m_lastSeenTime);
     }
 
     void Update()
     {
-        m_elapsed += Time.deltaTime;
-        if (m_elapsed >= bps)
+        float currentElapsed = audioSource.time;
+        int currentBeatIndex = (int)(currentElapsed / bps);
+        int lastBeatIndex = (int)(m_lastSeenTime / bps);
+        if (currentBeatIndex != lastBeatIndex)
         {
-            m_elapsed -= bps;
             OnBeat(this, null);
         }
+        m_lastSeenTime = currentElapsed;
     }
 }
