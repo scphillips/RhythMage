3bb0dd1ba26f776aec4c72c07600c6fa111a0240
Stephen Phillips <stephen.phillips.me@gmail.com>
2018-12-18 04:09:57 +0000

    Added skip behaviour to title scene.

--- Assets/Scripts/TitleSceneController.cs
+++ Assets/Scripts/TitleSceneController.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -12,25 +11,32 @@ public class TitleSceneController : MonoBehaviour
     readonly string nextScene = "MenuScene";
     readonly float m_fadeSpeed = 2.0f;
     float m_opacity;
+    bool m_isEnding;
 
 	void Start()
     {
         m_opacity = 0;
+        m_isEnding = false;
     }
 	
 	void Update()
     {
+        if (m_isEnding == false && m_opacity == 1.0f)
+        {
+            m_isEnding = (Input.anyKeyDown || audioSource.isPlaying == false);
+        }
+
         float mag = Time.deltaTime * m_fadeSpeed;
-        float delta = (audioSource.isPlaying) ? mag : -mag;
-        m_opacity += delta;
-		foreach (var entry in fadeEntities)
+        float delta = (m_isEnding) ? -mag : mag;
+        m_opacity = System.Math.Max(0.0f, System.Math.Min(1.0f, m_opacity + delta));
+        foreach (var entry in fadeEntities)
         {
             var color = entry.GetComponent<Image>().color;
             color.a = m_opacity;
             entry.GetComponent<Image>().color = color;
         }
 
-        if (m_opacity < 0.0f)
+        if (m_isEnding && m_opacity == 0.0f)
         {
             SceneManager.LoadScene(nextScene);
         }
